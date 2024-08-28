eee85c05340766e859af8d30ddc2e05980e02639
jeonjiwon <jiwon9609@naver.com>
2024-08-29 00:29:29 +0900

    init commit

---
+++ src/main/java/com/project/policyNews/PolicyNewsApplication.java
@@ -0,0 +1,13 @@
+package com.project.policyNews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolicyNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolicyNewsApplication.class, args);
+	}
+
+}
